f0d0dccc7795b1f1e6111927ee7e138b0f221818
cbolson13 <33496326+cbolson13@users.noreply.github.com>
2017-11-08 14:35:23 -0500

    Update MadLib_Chapter2.py

--- MadLib_Chapter2.py
+++ MadLib_Chapter2.py
@@ -1,10 +1,11 @@
 from MadLib_Chapter1 import *
+import time
 
 print("Chapter 2")
 print("Are you ready to begin your adventures? Yes/No")
 if ready == "Yes":
     print("Okay.")
-    print("My name is " + name1 + ", I have been the protector of " + city1 + "for the past six months.")
+    print("My name is " + name1 + ", I have been the protector of " + city1 + "for the last six months.")
     print("Type a noun.")
     noun11 = input().lower()
     print("Type a verb in the past tense.")
@@ -15,7 +16,7 @@ if ready == "Yes":
     adj11 = input().lower()
     print("Type an adjective.")
     adj12 = input().title()
-    print("Type a verb ending in -ing.")
+    print("Type an action verb ending in -ing.")
     verb13 = input().lower()
     print("Type a group of people.")
     name11 = input().lower()
@@ -23,10 +24,12 @@ if ready == "Yes":
     print("Last week my friends, " + name2 + ", " + name3 + " and I came across an evil " + noun11 + ".")
     print("When we encountered the evil " + noun11 + ",we " + verb11 + " it.")
     print("We quickly " + verb12 + " away in a " + adj11 + "without looking back.")
-    print("When we got back to the " + adj12 "quarters we were " + verb13 + "by our " + name11 ".")
-    print("")
+    print("When we got back to the " + adj12 + "quarters we were " + verb13 + "by our " + name11 ".")
+    print("But, we saw that " + noun11 + ", was waiting for us at " + adj12 + "quarters!" )
 
 elif ready == "No":
     print("Your loss.")
 else:
     print("That is not a valid answer.")
+
+    time.sleep(1000)
